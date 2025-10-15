8c1bf5379f43e00d185d4b8f5d8c52b5d6a760bb
luis <luis985849114@outlook.com>
2025-10-14 21:36:02 -0600

    PROYECTO INICIADO

---
+++ src/main/java/com/LP2_T2_LEON_LUIS/SPRING_EXAMEN_CRUD/SpringExamenCrudApplication.java
@@ -0,0 +1,13 @@
+package com.LP2_T2_LEON_LUIS.SPRING_EXAMEN_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExamenCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExamenCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LP2_T2_LEON_LUIS/SPRING_EXAMEN_CRUD/SpringExamenCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LP2_T2_LEON_LUIS.SPRING_EXAMEN_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamenCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
